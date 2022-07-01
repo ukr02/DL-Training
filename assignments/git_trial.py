c6123c02767a5296503e2fa660f063d455ed50ae
ukr02 <udaysripuram2002@gmail.com>
2022-07-01 13:07:25 +0530

    update

--- assignments/git_trial.py
+++ assignments/git_trial.py
@@ -2,3 +2,4 @@ import pandas as pd
 
 df = pd.read_csv("https://gist.githubusercontent.com/rnirmal/e01acfdaf54a6f9b24e91ba4cae63518/raw/6b589a5c5a851711e20c5eb28f9d54742d1fe2dc/datasets.csv")
 print(df.head())
+#change!
